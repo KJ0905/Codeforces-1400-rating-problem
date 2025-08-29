2e088964e65c36913d8931ab8a8db2ba9c81d9f0
KJ0905 <kapsm234@gmail.com>
2025-08-29 21:09:54 +0530

    Codeforces Round 641 (Div. 2)

---
+++ B. Orac and Models.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+//#include <math>
+//#include <string>
+using namespace std;
+#define ll long long
+#define ld long double
+#define forn(j,m) for(int i=j;i<m;i++)
+int mini=INT_MAX;
+int maxi=INT_MIN;
+ll lmini=INT64_MAX;
+ll lmaxi=INT64_MIN;
+#define mod 1000000007
+#define inf 1e9
+//freopen("guess.in","r",stdin);   freopen("billboard.out","w",stdout);
+#define all(x) (x).begin(), (x).end()  //sort(all(vec)) instead of sort(vec.begin(), vec.end()).
+#define al(x,n) x,x+n
+//#define rall(x) (x).rbegin(), (x).rend()
+#define KUNAL ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back 
+
+void solve(){
+    int n; cin>>n;
+    int a[n+1]; 
+    forn(1,n+1){cin>>a[i];}
+    vector<int>dp(n+1,1);
+    for(int i=1;i<=n;i++){
+        for(int j=i*2;j<=n;j+=i){
+            if(a[i]<a[j]){
+                dp[j]=max(dp[j],dp[i]+1);
+            }
+        }
+    }
+    int ans=0;
+    forn(1,n+1){ans=max(ans,dp[i]);}
+    cout<<ans<<endl;
+
+}  
+int main(){
+    KUNAL
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
